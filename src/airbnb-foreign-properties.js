726c6096c0751f0874ef51549e546cfe1b9c7e01
carlosseda <carlossedagambin@gmail.com>
2024-05-03 15:48:42 +0200

    Foreign properties

--- src/airbnb-foreign-properties.js
+++ src/airbnb-foreign-properties.js
@@ -23,18 +23,24 @@
 
   let foreignProperties = propertiesData.filter(element => element.host_location && countries.includes(element.host_location.split(', ')[1]))
 
-  console.log(foreignProperties.length)
-
   foreignProperties = foreignProperties.map(property => {
+
+    const regex = /^(\bETV|ET|VT)(?:\s*\/?)?\s*(\d+)$/i;
+    const transformedLicense = property.license.replace(regex, function(match, prefix, number) {
+      return `${prefix.toUpperCase()}/${number}`;
+    });
+
     return {
       hostName: property.host_name,
       hostCountry: property.host_location.split(', ')[1],
-      license: property.license,
+      license: transformedLicense,
       latitude: property.latitude,  
       longitude: property.longitude,
       location: property.neighbourhood_cleansed
     }
   }) 
+
+  const nonExistentProperties = []
 
   foreignProperties = foreignProperties.filter((property) => {
     let matchingElement = caibData.data.find(element => element[8] == property.license);
@@ -42,8 +48,12 @@
       property.manager = matchingElement[26];
       return true;
     }
+
+    nonExistentProperties.push(property)
+
     return false;
   });
 
-  console.log(foreignProperties.length)
+  fs.writeFile('../data/airbnb-non-license-foreign-properties.json', JSON.stringify(nonExistentProperties, null, 2))
+  fs.writeFile('../data/airbnb-foreign-properties.json', JSON.stringify(foreignProperties, null, 2))
 })()
